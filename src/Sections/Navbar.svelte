1c4a9141f5943636dc3b18fdd8b2de1346b7efd2
Joseph P Borne <josephpbourne@gmail.com>
2022-04-04 10:09:46 -0700

    learned about components and props

--- src/Sections/Navbar.svelte
+++ src/Sections/Navbar.svelte
@@ -1,1 +1,17 @@
-<nav>My NavBar</nav>
+<script>
+    export let title="Default title";
+</script>
+
+<style>
+    nav { 
+        width: 100%;
+        font-size: 2rem;
+        padding: 0.5rem 0;
+        background-color: #333;
+        color: #fff;
+        text-align: center;
+    }
+
+</style>
+
+<nav>{title}</nav>
