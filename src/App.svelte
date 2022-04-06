ade76b9467f0dac246e685052e3016606a7e61f3
Joseph P Borne <josephpbourne@gmail.com>
2022-04-05 20:12:11 -0700

    learned about reactivity and event handling with html elements using svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,23 @@
 <script>
 	import NavBar from "./Sections/Navbar.svelte"
 
-	let disabled = false;
+	let disabled = true;
+
+	function keyPressed() {
+		disabled = false
+	}
+
+	function resetClicked() {
+		disabled = true;
+	}
 </script>
 
 <NavBar title="vCard Generator" />
 
-<button {disabled}>Reset</button>
+<form>
+	<label>Name 
+		<input type="text" placeholder="Please enter your name"
+		on:keydown={keyPressed} />
+	</label>
+	<button type="reset" {disabled} on:click={resetClicked}>Reset</button>
+</form>
